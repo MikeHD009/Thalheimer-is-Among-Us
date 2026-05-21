959e5f32f1c11002e22b5919de727bf1cd7b3737
lb12a <leander.bergt@students.htl-ibk.at>
2026-05-21 12:39:14 +0200

    eaf
    sfegafw

--- Thalheimer is Among us/Assets/Map/Kachelsätze/Classroom_A2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="Classroom_A4" tilewidth="24" tileheight="24" tilecount="240" columns="16">
- <image source="../Map Assets/Classroom_A4.png" width="384" height="360"/>
-</tileset>
--- Thalheimer is Among us/Assets/Map/Kachelsätze/Classroom_A4.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="Classroom_A2" tilewidth="24" tileheight="24" tilecount="192" columns="16">
- <image source="../Map Assets/Classroom_A2.png" width="384" height="288"/>
-</tileset>
